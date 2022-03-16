dd9fd689652efb09ec1d81fd0cd376ce4dd6ab0f
MaetDol <rndka9@gmail.com>
2022-03-17 00:06:54 +0900

    fix: 응답 제대로 내려주게 변경

--- countrySearch_js/msw/src/api.js
+++ countrySearch_js/msw/src/api.js
@@ -1,20 +1,20 @@
 import { rest } from 'msw';
 import db from './db';
+import { baseUrl } from './utils';
 
-function url(path) {
-	return '/api' + path;
-}
-
-const searchCountry = rest.get(url`/search`, (req, res, ctx) => {
-	const keyword = req
-		.url.searchParams.get('keyword')
-		.toString()
-		.trim();
+const searchCountry = rest.get(baseUrl`/search`, (req, res, ctx) => {
+	let keyword = req.url.searchParams.get('keyword') || '';
+	keyword = keyword.trim().toLowerCase();
 	if(!keyword.length) return res(ctx.json([]));
 
-	return db
-		.getAllCountry()
-		.filter(country => country.includes(keyword));
+	return res(
+		ctx.delay(),
+		ctx.json(
+			db
+				.getAllCountry()
+				.filter(country => country.toLowerCase().includes(keyword))
+		)
+	);
 });
 
 export default [
